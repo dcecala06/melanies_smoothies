3b70db21347b281850140d56c4f4e1db96ca066e
dcecala06 <dantececala@outlook.com>
2026-05-17 20:51:14 -0500

    Add requests import and fetch smoothie data

--- streamlit_apps.py
+++ streamlit_apps.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests 
 # Import snowflake package
 from snowflake.snowpark.functions import col
 
@@ -27,6 +28,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
     #st.write(ingredients_string)
 
@@ -44,9 +47,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon ="☑️")
 
 
-#new section to display smoothiefroot nutrition information
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+ 
+
 
